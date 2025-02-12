b294375f559638302239ab2c4e4793acacf28a19
Joshua <joshwu10806@gmail.com>
2025-02-11 23:35:42 -0600

    add health bar

--- kill_zone.gd
+++ kill_zone.gd
@@ -5,10 +5,12 @@ extends Area2D
 
 func _on_body_entered(body) -> void:
 	if body == player:
-		player.Health -= 100
-		print(player.Health)
+		_deal_damage(100)
 	if player.Health < 0:
 		timer.start()
+		
+func _deal_damage(amount: int) -> void:
+	get_tree().get_nodes_in_group("health")[0]._update_health(100)
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://death_screen.tscn")
