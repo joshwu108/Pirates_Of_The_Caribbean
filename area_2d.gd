d4ac6e93c79520cd8b15dd0e39b592b956abdb7b
Joshua <joshwu10806@gmail.com>
2025-02-13 21:04:11 -0600

    fix error with level changing

--- area_2d.gd
+++ area_2d.gd
@@ -2,7 +2,5 @@ extends Area2D
 
 #@onready var enemy = get_tree().get_nodes_in_group("enemy")[0]
 
-func _on_body_entered(body: Node2D) -> void:
-	if body == get_tree().get_nodes_in_group("enemy")[0]:
-		get_tree().get_nodes_in_group("enemy")[0].health -= 100
-		print("damage_done")
+#func _on_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://Level_2.tscn")
